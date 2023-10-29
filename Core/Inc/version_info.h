9f88961175a7b7349f609eb1e29ef0c4062a7c6c
Yi-Syuan, Shen <yisyuanshen@gmail.com>
2023-10-29 14:44:32 +0800

    Update version information

--- Core/Inc/version_info.h
+++ Core/Inc/version_info.h
@@ -8,11 +8,11 @@
 #ifndef VERSION_INFO_H_
 #define VERSION_INFO_H_
 
-#define FIRMWARE_VERSION   "2.0.1"
+#define FIRMWARE_VERSION   "2.0.2"
 #define FIRMWARE_DATE      __DATE__
 #define FIRMWARE_TIME      __TIME__
 #define AUTHOR_NAME        "Yi-Syuan, Shen (r12522823)"
 
-#define MODIFICATION_INFO  "Modify Hardware and User Configurations"
+#define MODIFICATION_INFO  "Test all the modes and functions with debug board"
 
 #endif /* VERSION_INFO_H_ */
