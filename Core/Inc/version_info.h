e69e540986275b860334d5826a54934a3ac371ef
Yi-Syuan, Shen <yisyuanshen@gmail.com>
2023-11-01 17:10:59 +0800

    Update version info and re-build the project

--- Core/Inc/version_info.h
+++ Core/Inc/version_info.h
@@ -8,11 +8,11 @@
 #ifndef VERSION_INFO_H_
 #define VERSION_INFO_H_
 
-#define FIRMWARE_VERSION   "2.0.2"
+#define FIRMWARE_VERSION   "2.0.5"
 #define FIRMWARE_DATE      __DATE__
 #define FIRMWARE_TIME      __TIME__
 #define AUTHOR_NAME        "Yi-Syuan, Shen (r12522823)"
 
-#define MODIFICATION_INFO  "Fix the bug of position reply through CAN"
+#define MODIFICATION_INFO  "Test Hall Calibration Mode."
 
 #endif /* VERSION_INFO_H_ */
