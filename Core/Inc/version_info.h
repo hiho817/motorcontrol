980abe976367926ab4f1a71e5cdff379c824f46b
Yi-Syuan, Shen <yisyuanshen@gmail.com>
2023-11-09 18:43:19 +0800

    Update the firmware version

--- Core/Inc/version_info.h
+++ Core/Inc/version_info.h
@@ -8,11 +8,11 @@
 #ifndef VERSION_INFO_H_
 #define VERSION_INFO_H_
 
-#define FIRMWARE_VERSION   "3.0.3"
+#define FIRMWARE_VERSION   "4.0.1"
 #define FIRMWARE_DATE      __DATE__
 #define FIRMWARE_TIME      __TIME__
 #define AUTHOR_NAME        "Yi-Syuan, Shen (r12522823)"
 
-#define MODIFICATION_INFO  "Modify the condition of initializing the Hall Calibration Direction."
+#define MODIFICATION_INFO  "Update Firmware Version."
 
 #endif /* VERSION_INFO_H_ */
